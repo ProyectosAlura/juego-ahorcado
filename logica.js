0688210a46011ef4d5cc77f8ed6fd9fb9b8d3221
Daniel <danielcaro1000@gmail.com>
2023-02-16 11:46:54 -0500

    agregandp layout de agregar palabra

--- logica.js
+++ logica.js
@@ -1,9 +1,16 @@
 const containerInicio = document.querySelector("[data-container-inicio]");
+const containerAgregar = document.querySelector("[data-container-agregarPalabra]");
 
 /* Funciones */
 const ocultarContenedorPrincipal = (evento) => {
     containerInicio.style.display="none";
 }
 
-containerInicio.addEventListener("click",ocultarContenedorPrincipal);
+const ocultarContenedorAgregarPalabra = (evento) =>{
+    containerAgregar.style.display="none";
+}
 
+/* Eventos */
+
+containerInicio.addEventListener("click",ocultarContenedorPrincipal);
+containerAgregar.addEventListener("click",ocultarContenedorAgregarPalabra);
